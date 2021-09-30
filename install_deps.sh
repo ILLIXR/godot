ea5ae886ae85b13bdee593e278d2904e34385608
Samuel Grayson <sam@samgrayson.me>
2021-09-30 17:34:36 -0500

    Add install script

---
+++ install_deps.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+sudo apt-get install -y \
+     build-essential \
+     scons \
+     pkg-config \
+     libx11-dev \
+     libxcursor-dev \
+     libxinerama-dev \
+     libgl1-mesa-dev \
+     libglu-dev \
+     libasound2-dev \
+     libpulse-dev \
+     libudev-dev \
+     libxi-dev \
+     libxrandr-dev yasm
